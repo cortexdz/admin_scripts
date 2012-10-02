256d29cad20b88d765cf9318fe6156ca7b6498d3
Omar AKHAM <crtx.omar@gmail.com>
2012-10-02 14:03:32 +0100

    update owncloud script

--- owncloud_install.sh
+++ owncloud_install.sh
@@ -8,6 +8,8 @@ _WWW="/srv/www"
 # Install Lamp
 apt-get install apache2 php5 mysql-server php5-gd php5-mysql php5-common mp3info curl libcurl3 libcurl4-openssl-dev php5-curl zip bzip2
 
+a2enmod rewrite ssl headers
+
 # Download latest
 wget "http://owncloud.org/releases/owncloud-latest.tar.bz2" -O /tmp/owncloud-latest.tar.bz2
 tar -xjf /tmp/owncloud-latest.tar.bz2
