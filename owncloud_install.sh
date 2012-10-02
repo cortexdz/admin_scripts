2580f037ddccc29e5eb5fbb65ec0c85595e493c8
Omar AKHAM <crtx.omar@gmail.com>
2012-10-02 12:04:32 +0100

    fix _WWW : remove last slash

--- owncloud_install.sh
+++ owncloud_install.sh
@@ -3,7 +3,7 @@
 ## Install Owncloud on Fresh Debian Squeeze
 ## Source : http://owncloud.org/support/install/
 
-_WWW="/srv/www/"
+_WWW="/srv/www"
 
 # Install Lamp
 apt-get install apache2 php5 mysql-server php5-gd php5-mysql php5-common mp3info curl libcurl3 libcurl4-openssl-dev php5-curl zip bzip2
@@ -12,5 +12,5 @@ apt-get install apache2 php5 mysql-server php5-gd php5-mysql php5-common mp3info
 wget "http://owncloud.org/releases/owncloud-latest.tar.bz2" -O /tmp/owncloud-latest.tar.bz2
 tar -xjf /tmp/owncloud-latest.tar.bz2
 mkdir $_WWW
-mv owncloud $_WWW
-chown -R www-data:www-data $_WWW\owncloud
+mv owncloud $_WWW/
+chown -R www-data:www-data $_WWW/owncloud
